b05e26efc6313aab0b9a97d5491b751794da28f5
Oluchi29 <oluchiapere29@gmail.com>
2025-04-08 09:57:25 +0100

    file-update

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "demo-bucket-2010"
     region = "us-east-1"
-    key    = "infra-backend/terraform.tfstate"
+    key    = "backend-1/terraform.tfstate"
   }
 }
